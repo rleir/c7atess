0d19de46d5949ee10d0b3141151f7b372410fba2
Richard Leir <richard.leir@canadiana.ca>
2014-09-16 14:44:31 +0000

    reorder the fields add a unique key

--- ocrResults.sql
+++ ocrResults.sql
@@ -9,22 +9,19 @@ USE `mydb` ;
 -- Table `mydb`.`ocr`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`ocr` (
-  `idocr` INT NOT NULL AUTO_INCREMENT,
+  `idocr` INT NOT NULL,
   `imageFile` VARCHAR(200) NOT NULL,
-  `ocrEngine` VARCHAR(45) NOT NULL,
-  `langParam` VARCHAR(8) NOT NULL,
-  `brightness` INT NOT NULL,
-  `contrast` INT NOT NULL,
   `avgWordConfidence` INT NULL,
   `numWords` INT NULL,
   `startOcr` DATETIME NULL,
   `timeOcr` INT NULL,
+  `ocrEngine` VARCHAR(45) NULL,
   `remarks` VARCHAR(45) NULL,
   `imageFileSize` INT NULL,
+  `langParam` VARCHAR(8) NULL,
   `outputText` TEXT NULL,
-  `outputHocr` BLOB NULL,
-  PRIMARY KEY (`idocr`),
-  UNIQUE INDEX `index2` USING BTREE (`imageFile` ASC, `ocrEngine` ASC, `brightness` ASC, `contrast` ASC, `langParam` ASC))
+  `outputHocr` TEXT NULL,
+  PRIMARY KEY (`idocr`, `imageFile`(200)))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
