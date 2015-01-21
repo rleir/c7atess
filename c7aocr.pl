ec143ad63ccc6460cc6b1f7a471ae7494ac90abf
Richard Leir <richard.leir@canadiana.ca>
2015-01-21 16:02:19 +0000

    only use 6 cores on aragon, to allow mysql to perform better

--- c7aocr.pl
+++ c7aocr.pl
@@ -66,7 +66,7 @@ my $fileTypes  = " -name \\*.jpg -o -name \\*.jp2 -o -name \\*.tif ";
 
 # jobs are distributed to the 'eight' server and are also run on the local machine.
 # an arbitrary (yikes) delay saves ssh from being 'overwhelmed'.
-my $serverList = " -S richard\\\@darcy-pc -S richard\\\@yb -S richard\\\@xynotyro -S richard\\\@aragon -S richard\\\@zamorano -S : --sshdelay 0.2 ";
+my $serverList = " -S richard\\\@darcy-pc -S richard\\\@yb -S richard\\\@xynotyro -S 6/richard\\\@aragon -S richard\\\@zamorano -S : --sshdelay 0.2 ";
 
 # xynotyro.office.c7a.ca
 
