449954cec19c12602ab22670916a2df46da8770d
Richard Leir <richard.leir@canadiana.ca>
2015-07-06 13:50:09 +0000

    this should have been checked in long ago. the indexing is better now

--- ocrResults.sql
+++ ocrResults.sql
@@ -9,19 +9,22 @@ USE `mydb` ;
 -- Table `mydb`.`ocr`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`ocr` (
-  `idocr` INT NOT NULL,
+  `idocr` INT NOT NULL AUTO_INCREMENT,
   `imageFile` VARCHAR(200) NOT NULL,
+  `ocrEngine` VARCHAR(45) NOT NULL,
+  `langParam` VARCHAR(8) NOT NULL,
+  `brightness` INT NULL,
+  `contrast` INT NULL,
   `avgWordConfidence` INT NULL,
   `numWords` INT NULL,
   `startOcr` DATETIME NULL,
   `timeOcr` INT NULL,
-  `ocrEngine` VARCHAR(45) NULL,
   `remarks` VARCHAR(45) NULL,
   `imageFileSize` INT NULL,
-  `langParam` VARCHAR(8) NULL,
   `outputText` TEXT NULL,
-  `outputHocr` TEXT NULL,
-  PRIMARY KEY (`idocr`, `imageFile`(200)))
+  `outputHocr` BLOB NULL,
+  PRIMARY KEY (`idocr`),
+  UNIQUE INDEX `index2` USING BTREE (`imageFile` ASC, `ocrEngine` ASC, `brightness` ASC, `contrast` ASC, `langParam` ASC))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
